d2cb6c0a861d4c5b955e24a33a84d7edcdbcac4b
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-20 12:08:46 +0545

    done

---
+++ task06.py
@@ -0,0 +1,12 @@
+number = input("enter number:")
+
+password = number[:2]
+
+if password == "90" or password == "91":
+    print("ucell")
+if password == "93" or password == "94":
+    print("beeline")
+if password == "88" or password == "99":
+    print("mobile uz")
+else:
+    print(" nomalum raqam")
