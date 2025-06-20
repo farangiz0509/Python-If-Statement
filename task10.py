9c617960b5d2fe8e26fa41e79c45f0fbc848cd89
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-20 13:07:37 +0545

    done

---
+++ task10.py
@@ -0,0 +1,13 @@
+rating = int(input("enter the score"))
+
+if rating > 90 and rating < 100:
+    print("A")
+    
+if rating > 80 and rating < 90:
+    print("B")
+if rating >70 and rating < 80:
+    print("c")
+if rating > 60 and rating < 70:
+    print("D")
+if rating > 0 and rating < 60:
+    print("E")
