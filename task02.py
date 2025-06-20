8d1bbf7a8859376dc4fa93e21e662394ee3b8c08
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-20 10:31:32 +0545

    done

---
+++ task02.py
@@ -0,0 +1,9 @@
+number1 = int(input("enter number1:"))
+number2 = int(input("enter number2:"))
+
+if number1 > number2 :
+    print("birinchi son katta" , number1)
+else:
+    print("ikkinchi son katta", number2)
+if number1 == number2 :
+    print("ikkala son teng")
