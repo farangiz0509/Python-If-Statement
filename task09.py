a417a4528c1bd27bf4f5fd8e50e8c684fd0a0979
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-20 12:57:28 +0545

    done

---
+++ task09.py
@@ -0,0 +1,10 @@
+a = int(input(" 1 tomonini kiriting"))
+b = int(input("2 tomonini kiritig"))
+c = int(input("3 tomonini kiriting"))
+
+if a == b == c :
+    print("teng tomonli")
+if a == b or b == c or a == c:
+    print("teng yonli")
+else:
+    print("turli tomonli")
