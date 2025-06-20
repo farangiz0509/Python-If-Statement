4011c6061ede8e3b4aec33774d75c6d658e41103
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-20 13:19:09 +0545

    done

---
+++ task12.py
@@ -0,0 +1,8 @@
+password = input("enter the password:")
+
+password2 = input("enter the password:")
+
+if password == password2:
+    print("password is correct")
+else:
+    print("password is uncorrect")
